c606ae3c8fe67f1b21859fa5337857433362d423
Dean Gelbaum <deanscottg@gmail.com>
2020-07-31 19:25:26 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,6 +2,9 @@ def square_array(array)
   new_square_array = []
   i = 0 
  while i < array.length do 
+   squared_number = array[i] ** 2 
+   new_square_array.push(squared_number)
+   
    
    
  end
